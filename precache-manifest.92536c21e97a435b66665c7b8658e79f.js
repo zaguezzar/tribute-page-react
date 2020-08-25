bddcbb5ca42fed4e3e5360f21bad8aaeb4857668
zaguezzar <zaguezzar@outlook.com>
2020-08-25 23:21:55 +0100

    Updates

---
+++ precache-manifest.92536c21e97a435b66665c7b8658e79f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b23efdee38e7e527666093ee2349f8d",
+    "url": "/tribute-page-react/index.html"
+  },
+  {
+    "revision": "2e1b7d19908eeb8399e2",
+    "url": "/tribute-page-react/static/js/2.997fce4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tribute-page-react/static/js/2.997fce4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f8fcdb215519ad9fa9",
+    "url": "/tribute-page-react/static/js/main.cb6b31ab.chunk.js"
+  },
+  {
+    "revision": "96331ac2540248aed30f",
+    "url": "/tribute-page-react/static/js/runtime-main.6b0c4f5e.js"
+  }
+]);
